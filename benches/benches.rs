f018fa65280bf7d65d30840470b6c1ec59968d89
Saúl Valdelvira <saul@saulv.es>
2024-11-29 17:02:16 +0100

    Add more benches

--- benches/benches.rs
+++ benches/benches.rs
@@ -18,11 +18,47 @@ fn compile(b: &mut Bencher) {
 }
 
 #[bench]
-fn test(b: &mut Bencher) {
+fn matches(b: &mut Bencher) {
     let huge_regex = format!("({HUGE_REGEX_A}|{HUGE_REGEX_B})");
     let regex = Regex::compile(&huge_regex).unwrap();
     b.iter(|| {
         assert!(regex.test(HUGE_REGEX_A));
         assert!(regex.test(HUGE_REGEX_B));
+    });
+}
+
+#[bench]
+fn non_match(b: &mut Bencher) {
+    let huge_regex = format!("({HUGE_REGEX_A}|{HUGE_REGEX_B})");
+    let regex = Regex::compile(&huge_regex).unwrap();
+    b.iter(|| {
+        assert!(!regex.test("abclkaj"));
+    });
+}
+
+#[bench]
+fn non_match_huge(b: &mut Bencher) {
+    let huge_regex = format!("({HUGE_REGEX_A}|{HUGE_REGEX_B})");
+    let regex = Regex::compile(&huge_regex).unwrap();
+    b.iter(|| {
+        assert!(!regex.test("pppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppp"));
+    });
+}
+
+#[bench]
+fn non_match_strict_start_end(b: &mut Bencher) {
+    let huge_regex = format!("^({HUGE_REGEX_A}|{HUGE_REGEX_B})$");
+    let regex = Regex::compile(&huge_regex).unwrap();
+    b.iter(|| {
+        assert!(!regex.test("abclkaj"));
+    });
+}
+
+#[bench]
+fn non_match_huge_strict_start_end(b: &mut Bencher) {
+    let huge_regex = format!("^({HUGE_REGEX_A}|{HUGE_REGEX_B})$");
+    let regex = Regex::compile(&huge_regex).unwrap();
+    b.iter(|| {
+        assert!(!regex.test("pppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppp"));
     });
 }
