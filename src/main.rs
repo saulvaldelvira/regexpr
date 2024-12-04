011fdf397d145a051d641fa68b73b36788b5782b
Saúl Valdelvira <saul@saulv.es>
2024-12-04 08:58:53 +0100

    regexpr (program): Loop over the program's login

--- src/main.rs
+++ src/main.rs
@@ -3,30 +3,34 @@ use std::io::{stdin, stdout, Write};
 use regexpr::Regex;
 
 fn main() {
-    print!("Enter a regular expression: ");
-    stdout().flush().unwrap();
+    loop {
+        print!("Enter a regular expression: ");
+        stdout().flush().unwrap();
 
-    let mut buf = String::new();
-    stdin().read_line(&mut buf).unwrap();
-    let buf = buf.trim().replace("\n", "").replace("\r", "");
+        let mut buf = String::new();
+        stdin().read_line(&mut buf).unwrap();
+        let buf = buf.trim().replace("\n", "").replace("\r", "");
 
-    let regex = Regex::compile(&buf).expect("Invalid regex");
+        let regex = Regex::compile(&buf).expect("Invalid regex");
 
-    print!("> ");
-    stdout().flush().unwrap();
-
-    stdin().lines().map_while(Result::ok).for_each(|line| {
-        let it = regex.find_matches(&line);
-        if it.clone().next().is_none() {
-            println!("No matches");
-        } else {
-            println!("=== Matches ===");
-            for (i,m) in it.enumerate() {
-                println!("{}) {m}", i + 1);
-            }
-            println!("===============");
-        }
         print!("> ");
         stdout().flush().unwrap();
-    });
+
+        stdin().lines().map_while(Result::ok).for_each(|line| {
+            let it = regex.find_matches(&line);
+            if it.clone().next().is_none() {
+                println!("No matches");
+            } else {
+                println!("=== Matches ===");
+                for (i,m) in it.enumerate() {
+                    println!("{}) {m}", i + 1);
+                }
+                println!("===============");
+            }
+            print!("> ");
+            stdout().flush().unwrap();
+        });
+
+        println!();
+    }
 }
