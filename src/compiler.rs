8d816c7d647d1cda7bea4b783a9a1af2734fd79e
Saúl Valdelvira <saul@saulv.es>
2025-01-03 16:49:21 +0100

    Regex compiler: Split logic into different functions

--- src/compiler.rs
+++ src/compiler.rs
@@ -14,6 +14,8 @@ pub struct RegexCompiler<'a> {
     n_captures: usize
 }
 
+type Result<T> = std::result::Result<T,Cow<'static,str>>;
+
 impl<'a> RegexCompiler<'a> {
     pub fn new(src: &'a str) -> Self {
         let mut compiler = RegexCompiler {
@@ -62,51 +64,139 @@ impl<'a> RegexCompiler<'a> {
             unreachable!()
         })
     }
-    #[allow(clippy::too_many_lines)]
-    pub fn process(&mut self) -> Result<Regex,Cow<'static,str>> {
-        while let Some(c) = self.chars.next() {
-            macro_rules! next {
-                () => {
-                    {
-                        let c = self.chars.next().ok_or_else(|| format!("Expected character after {c}"))?;
-                        c
-                    }
-                };
+    fn next(&mut self, c: char) -> Result<char> {
+        self.chars
+            .next()
+            .ok_or_else(|| format!("Expected character after {c}").into())
+    }
+    fn multiplier(&mut self, c: char) -> Result<MatchCase> {
+        let last = self.last_acc().0.pop()
+            .ok_or_else(|| format!("Expected pattern before '{c}'"))?;
+        let last = Box::new(last);
+
+        let lazy = self.chars.clone().next().is_some_and(|c| c == '?');
+        if lazy { self.chars.next(); }
+
+        let case = match c {
+            '?' => MatchCase::Opt(last),
+            '+' => MatchCase::OneOrMore { case: last, lazy },
+            '*' => MatchCase::Star { case: last, lazy },
+            _ => unreachable!()
+        };
+        Ok(case)
+    }
+    fn repeat(&mut self, c: char) -> Result<MatchCase> {
+        let last = self.last_acc().0.pop()
+            .ok_or_else(|| format!("Expected pattern before '{c}'"))?;
+
+        /* a{100,1000} */
+
+        let i = self.chars.as_str().find('}').ok_or("Missing closing '}'")?;
+        let slice = &self.chars.as_str()[..i];
+        let mut split = slice.split(',');
+        let min = split.next().ok_or("Range must be split by ','. Ex: {12,15}")?;
+        let max = split.next().ok_or("Range must be split by ','. Ex: {12,15}")?;
+
+        let min = if min.is_empty() { None } else { Some(min.parse().ok().ok_or("Error parsing number")?) };
+        let max = if max.is_empty() { None } else { Some(max.parse().ok().ok_or("Error parsing number")?) };
+
+        for _ in 0..=i {
+            self.chars.next();
+        }
+
+        Ok(MatchCase::RangeLoop { case: Box::new(last), min, max })
+    }
+    fn range(&mut self, c: char) -> Result<MatchCase> {
+        let mut curr = self.next(c)?;
+        let negated = curr == '^';
+        if negated {
+            curr = self.next(c)?;
+        }
+
+        let mut list = Vec::new();
+
+        while curr != ']' {
+            if curr == '\\' {
+                curr = self.next(c)?;
             }
+            let c = curr;
+            curr = self.next(c)?;
+
+            if curr == '-' {
+                let end = self.next(c)?;
+                if end == ']' {
+                    return Err("Expectend end of range [.. - ..]".into());
+                }
+                list.push(MatchCase::Between(c,end));
+                curr = self.next(c)?;
+            } else {
+                list.push(MatchCase::Char(c));
+            }
+        }
+
+        let match_case = list.into_boxed_slice();
+        let case = if negated {
+            MatchCase::Not(Box::new(MatchCase::CharMatch(match_case)))
+        } else {
+            MatchCase::CharMatch(match_case)
+        };
+        Ok(case)
+    }
+    fn or(&mut self) {
+        match self.accc.pop() {
+            Some((mut acc, mut opt, cid)) => {
+                let m = if acc.len() > 1 {
+                    MatchCase::List(acc.into_boxed_slice())
+                } else {
+                    acc.remove(0)
+                };
+                opt.get_or_insert_with(Vec::new).push(m);
+                self.accc.push((Vec::new(), opt, cid));
+            },
+            None => unreachable!(),
+        };
+    }
+    fn escape(&mut self, c: char) -> Result<MatchCase> {
+        let mut is_cap = self.chars.clone().next().is_some_and(char::is_numeric);
+
+        let mut arrrows = false;
+        if !is_cap && self.chars.as_str().strip_prefix("k<").is_some() {
+            self.chars.next();
+            self.chars.next();
+            is_cap = true;
+            arrrows = true;
+        }
+
+        let case =
+        if is_cap {
+            let mut captn = 0;
+            while let Some(n) = self.chars.clone().next() {
+                if !n.is_numeric() {
+                    if arrrows && self.next(c)? != '>' {
+                        return Err("Expected closing '>'".into())
+                    }
+                    break
+                }
+
+                captn = captn * 10 + (n as u8 - b'0') as usize;
+
+                self.chars.next();
+            }
+            if self.n_captures < captn {
+                return Err("Trying to recall uncaptured".into());
+            }
+            MatchCase::Capture(captn)
+        } else {
+            MatchCase::Char(self.next(c)?)
+        };
+        Ok(case)
+    }
+    pub fn process(&mut self) -> Result<Regex> {
+        while let Some(c) = self.chars.next() {
             let newcase = match c {
                 '.' => MatchCase::AnyOne,
                 '\\' => {
-                    let mut is_cap = self.chars.clone().next().is_some_and(char::is_numeric);
-
-                    let mut arrrows = false;
-                    if !is_cap && self.chars.as_str().strip_prefix("k<").is_some() {
-                        self.chars.next();
-                        self.chars.next();
-                        is_cap = true;
-                        arrrows = true;
-                    }
-
-                    if is_cap {
-                        let mut captn = 0;
-                        while let Some(n) = self.chars.clone().next() {
-                            if !n.is_numeric() {
-                                if arrrows && next!() != '>' {
-                                    return Err("Expected closing '>'".into())
-                                }
-                                break
-                            }
-
-                            captn = captn * 10 + (n as u8 - b'0') as usize;
-
-                            self.chars.next();
-                        }
-                        if self.n_captures < captn {
-                            return Err("Trying to recall uncaptured".into());
-                        }
-                        MatchCase::Capture(captn)
-                    } else {
-                        MatchCase::Char(next!())
-                    }
+                    self.escape(c)?
                 },
                 '(' => {
                     self.enter_scope(true);
@@ -116,90 +206,17 @@ impl<'a> RegexCompiler<'a> {
                     self.close_scope()
                 }
                 '|' => {
-                    match self.accc.pop() {
-                        Some((mut acc, mut opt, cid)) => {
-                            let m = if acc.len() > 1 {
-                                MatchCase::List(acc.into_boxed_slice())
-                            } else {
-                                acc.remove(0)
-                            };
-                            opt.get_or_insert_with(Vec::new).push(m);
-                            self.accc.push((Vec::new(), opt, cid));
-                        },
-                        None => unreachable!(),
-                    };
+                    self.or();
                     continue;
                 },
                 '[' => {
-                    let mut curr = next!();
-                    let negated = curr == '^';
-                    if negated {
-                        curr = next!();
-                    }
-
-                    let mut list = Vec::new();
-
-                    while curr != ']' {
-                        if curr == '\\' {
-                            curr = next!();
-                        }
-                        let c = curr;
-                        curr = next!();
-
-                        if curr == '-' {
-                            let end = next!();
-                            if end == ']' {
-                                return Err("Expectend end of range [.. - ..]".into());
-                            }
-                            list.push(MatchCase::Between(c,end));
-                            curr = next!();
-                        } else {
-                            list.push(MatchCase::Char(c));
-                        }
-                    }
-
-                    let match_case = list.into_boxed_slice();
-                    if negated {
-                        MatchCase::Not(Box::new(MatchCase::CharMatch(match_case)))
-                    } else {
-                        MatchCase::CharMatch(match_case)
-                    }
+                    self.range(c)?
                 },
                 '{' => {
-                    let last = self.last_acc().0.pop()
-                               .ok_or_else(|| format!("Expected pattern before '{c}'"))?;
-
-                    /* a{100,1000} */
-
-                    let i = self.chars.as_str().find('}').ok_or("Missing closing '}'")?;
-                    let slice = &self.chars.as_str()[..i];
-                    let mut split = slice.split(',');
-                    let min = split.next().ok_or("Range must be split by ','. Ex: {12,15}")?;
-                    let max = split.next().ok_or("Range must be split by ','. Ex: {12,15}")?;
-
-                    let min = if min.is_empty() { None } else { Some(min.parse().ok().ok_or("Error parsing number")?) };
-                    let max = if max.is_empty() { None } else { Some(max.parse().ok().ok_or("Error parsing number")?) };
-
-                    for _ in 0..=i {
-                        self.chars.next();
-                    }
-
-                    MatchCase::RangeLoop { case: Box::new(last), min, max }
+                    self.repeat(c)?
                 },
                 '?' | '*' | '+' => {
-                    let last = self.last_acc().0.pop()
-                               .ok_or_else(|| format!("Expected pattern before '{c}'"))?;
-                    let last = Box::new(last);
-
-                    let lazy = self.chars.clone().next().is_some_and(|c| c == '?');
-                    if lazy { self.chars.next(); }
-
-                    match c {
-                        '?' => MatchCase::Opt(last),
-                        '+' => MatchCase::OneOrMore { case: last, lazy },
-                        '*' => MatchCase::Star { case: last, lazy },
-                        _ => unreachable!()
-                    }
+                    self.multiplier(c)?
                 },
                 '^' => MatchCase::Start,
                 '$' => MatchCase::End,
@@ -207,6 +224,7 @@ impl<'a> RegexCompiler<'a> {
             };
             self.append(newcase);
         };
+
         let matches = match self.close_scope() {
             MatchCase::List(cases) =>  cases,
             MatchCase::Or(l) => Box::from([MatchCase::Or(l)]),
