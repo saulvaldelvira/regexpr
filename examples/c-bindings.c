6c86dc6e95dc0fff1d41bf0bfe6ae24fe629aab4
Saúl Valdelvira <saul@saulv.es>
2025-01-03 22:02:02 +0100

    FFI: Add regex matcher bindings

--- examples/c-bindings.c
+++ examples/c-bindings.c
@@ -2,18 +2,36 @@
 #include <stdio.h>
 
 int main(void) {
-        Regex *regex = regex_compile("abc.*");
+        Regex *regex = regex_compile("(abc|def)");
 
         char* tests[] = {
                 "abc",
                 "abcc",
+                "abcabc",
+                "abcdefabc",
                 "abcdds",
                 NULL
         };
 
-        for (char **ptr = tests; *ptr; ptr++) {
-                printf("%s => %d\n", *ptr, regex_test(regex, *ptr));
-        }
+        printf("Regular expression: (abc|def)\n");
 
+        for (char **ptr = tests; *ptr; ptr++) {
+                bool matches = regex_test(regex, *ptr);
+                if (!matches)
+                        continue;
+                char *test = *ptr;
+                Span span;
+                RegexMatcher *matcher = regex_find_matches(regex, test);
+                printf("Matches of %s\n", test);
+                while (regex_matcher_next(matcher, &span)) {
+                        printf("[%d:%d] %.*s\n",
+                                (int)span.offset,
+                                (int)(span.offset + span.len),
+                                (int)span.len,
+                                &test[span.offset]
+                        );
+                }
+                regex_matcher_free(matcher);
+        }
         regex_free(regex);
 }
--- src/ffi.rs
+++ src/ffi.rs
@@ -1,9 +1,8 @@
 //! C bindings
 
-use std::ffi::{c_char, CStr};
+use std::ffi::{c_char, c_ulong, CStr};
 use std::ptr;
-
-use crate::Regex;
+use crate::{Regex, RegexMatcher};
 
 /// Compile the given string into a regex
 ///
@@ -36,6 +35,64 @@ fn regex_test(regex: *const Regex, src: *const c_char) -> bool {
     unsafe { &*regex } .test(src)
 }
 
+/// Returns an iterator over all the matches found in the source string
+///
+/// # Safety
+/// Ensure that.
+/// 1) regex is a valid pointer to a Regex struct
+/// 2) src is a valid NULL terminated C-String
+/// 3) You call `regex_matcher_free` on the returned pointer after you're done
+#[no_mangle]
+pub unsafe extern "C"
+fn regex_find_matches(regex: *const Regex, src: *const c_char) -> *mut RegexMatcher<'static> {
+    let src = unsafe { CStr::from_ptr(src) };
+    let Ok(src) = src.to_str() else { return ptr::null_mut() };
+
+    let matcher = unsafe { &*regex } .find_matches(src);
+    let matcher = Box::new(matcher);
+    Box::into_raw(matcher)
+}
+
+#[repr(C)]
+pub struct Span {
+    offset: c_ulong,
+    len: c_ulong,
+}
+
+/// Gets the next match from the matcher.
+/// Span is filled with the offset and len of the match
+///
+/// Returns true if there's another match, false if the iterator is over
+///
+/// # Safety
+/// Ensure that.
+/// 1) matcher is a valid pointer to a `RegexMatcher`
+/// 2) span is a valid pointer to a Span struct
+#[no_mangle]
+pub unsafe extern "C"
+fn regex_matcher_next(matcher: *mut RegexMatcher<'_>, span: *mut Span) -> bool {
+    match unsafe { &mut *matcher } .next() {
+        Some(m) => {
+            *span = Span {
+                offset: m.span().0 as c_ulong,
+                len: m.slice().len() as c_ulong,
+            };
+            true
+        },
+        None => false
+    }
+}
+
+/// Frees the regex matcher
+///
+/// # Safety
+/// Ensure that matcher is a valid pointer
+#[no_mangle]
+pub unsafe extern "C"
+fn regex_matcher_free(matcher: *mut RegexMatcher<'_>) {
+    drop( Box::from_raw(matcher) );
+}
+
 /// Frees the regex structure
 ///
 /// # Safety
@@ -45,5 +102,5 @@ fn regex_test(regex: *const Regex, src: *const c_char) -> bool {
 pub unsafe extern "C"
 fn regex_free(regex: *mut Regex) {
     let r = Box::from_raw(regex);
-    drop(r)
+    drop(r);
 }
